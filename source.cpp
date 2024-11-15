7cee4c6d50244f8d71ba55aa152e0836c41901b5
DinhThai03 <thai.05.10.2001@gmail.com>
2024-11-15 17:50:44 +0700

    tich

--- source.cpp
+++ source.cpp
@@ -3,10 +3,19 @@
 using namespace std;
 
 int tinhTong(int a, int b);
-int tinhTich(int a, int b);
+int tinhTich(int a, int b)
+{
+	return a * b;
+}
 int tinhHieu(int a, int b);
 
 void main()
 {
+	int a, b;
+	cout << "Nhap a: ";
+	cin >> a;
+	cout << "nhap b:";
+	cin >> b;
 
+	cout << "Tich la: " << tinhTich(a, b);
 }
