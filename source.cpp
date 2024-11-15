d12082e520f6245e556b3d0e3efb360740fc98d2
zHyuhz <149188400+zHyuhz@users.noreply.github.com>
2024-11-15 18:00:13 +0700

    thuong

--- source.cpp
+++ source.cpp
@@ -7,13 +7,17 @@ int tinhTich(int a, int b);
 int tinhHieu(int a, int b){
   return a-b;
 }
+int tinhThuong(int a, int b){
+	return a/b;
+}
 
 void main()
 {
 cout<<"Nhap a";
 cin>>a;
-cout<<"Nhap b";
-cin>>b;
+
 cout<<"Hieu la " << tinhHieu(a,b) << endln;
+
+cout << "Thuong la: " << tinhThuong(a,b);
 
 }
