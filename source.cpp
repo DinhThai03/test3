5b10df1538bc6ad66b459b9777bf233e4aa117ad
Khoa Truong <thukhoa1011@gmail.com>
2024-11-15 18:20:39 +0700

    co loi nhe

--- source.cpp
+++ source.cpp
@@ -5,28 +5,18 @@ using namespace std;
 int tinhTong(int a, int b) {
 	return a + b;
 
-int tinhTich(int a, int b)
-{
-	return a * b;
-}
-int tinhHieu(int a, int b){
-  return a-b;
-}
-int tinhThuong(int a, int b){
-	return a/b;
-}
+
 
 void main()
 {
 	int a, b;
 	cout << "Nhap a: ";
-	cin >> a;
-	cout << "nhap b:";
+
 	cin >> b;
 
-	cout << "Tong la: " << tinhTong();
-	cout << "Thuong la: " << tinhThuong(a,b);
 	cout << "Tich la: " << tinhTich(a, b);
 	cout<<"Hieu la " << tinhHieu(a,b) << endln;
+	cout << "Thang lon HUy";
+	int c, d;
 
 }
