b9444314c5e3b8af763077080d5ca57e1263a098
zHyuhz <149188400+zHyuhz@users.noreply.github.com>
2024-11-15 17:50:29 +0700

    Tim Hieu

--- source.cpp
+++ source.cpp
@@ -4,9 +4,16 @@ using namespace std;
 
 int tinhTong(int a, int b);
 int tinhTich(int a, int b);
-int tinhHieu(int a, int b);
+int tinhHieu(int a, int b){
+  return a-b;
+}
 
 void main()
 {
+cout<<"Nhap a";
+cin>>a;
+cout<<"Nhap b";
+cin>>b;
+cout<<"Hieu la " << tinhHieu(a,b) << endln;
 
 }
